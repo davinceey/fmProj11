5de288a4289e7d1a3541669e794403d6e34d4965
Daniel Vincent <danielvincent940@gmail.com>
2022-09-26 16:17:13 +0100

    modified files

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const shareButton = document.querySelector(".icon__share");
 const media = document.querySelector(".social__media");
 const body = document.querySelector("body");
+const overLay = document.querySelector(".overlay");
+let isDisplay = false;
 
-shareButton.addEventListener("click", ()=>{
-    // if(media.classList.contains("active")){
-    //     body.addEventListener("click", ()=>{
-    //         media.classList.remove("active");
-    //     })
-        
-    // }
+
+shareButton.addEventListener("click", ()=>{  
+
+    if(media.classList.contains("active")){
+        overLay.addEventListener("click", removeMedia);
+    }
     toggleMedia();
 })
+
 function toggleMedia(){
-    media.classList.toggle("active");
+        media.classList.toggle("active");
+        overLay.classList.toggle("lay");
 }
 
+function removeMedia(){
+    media.classList.remove("active");
+    overLay.classList.remove("lay");
+}
+    
